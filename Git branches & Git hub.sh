d1934bbb86d08c7e851bd8c23aaa1c4d87a6a477
Olanrewaju Muhammed YUSUF <yusufolanrewaju9133@gmail.com>
2022-11-06 22:55:25 +0100

    Git branches & Git hub.sh

---
+++ Git branches & Git hub.sh
@@ -0,0 +1,20 @@
+To ssh using visual studio code ssh -i <Copy key> ec2-user@<IPaddress>
+git log
+git log --oneline   -----Summarize the git commit and message in just one line
+SHA ----The first 7 characters of the commit ID used as an identifier by git
+Master branch is created when we initialize git in out working directory
+git branch ---To check your working branch
+git branch <branchname> ----To create a new branch
+git switch <branchname>   or git checkout <branchname> -------To switch to a branch
+git merge <branchname> -----To merge changes from new branch to master.
+git checkout -d <branchname> .... To delete a branch
+git checkout -b <branchname>   or git switch -c <branchname> -----To create and switch to a new branch in one command
+create a git hub account and create a repo
+git push -u origin master/main
+enter username and password
+access token
+git fetch
+git merge origin/master
+git pull
+git clone is used during intialization to copy the repo to your working directory
+forking a repo creates a copy of the repo in your remote repository
